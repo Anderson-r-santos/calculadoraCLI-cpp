b98b5c0032477b08e3678006a9b31221d792e322
anderson <anderson_r_santos17@hotmail.com>
2022-11-07 00:00:06 -0300

    criando print para guiar o usuario

--- main.cpp
+++ main.cpp
@@ -40,10 +40,19 @@ int main()
     int number1,number2;
     char operacao;
 
+    std::cout<<"Digite um numero :\n";
+
     std::cin>>number1;
-    std::cin>>number2;
+
+    std::cout<<"Digite o operando :\n";
+
     std::cin>>operacao;
+
+    std::cout<<"Digite o segundo numero :\n";
     
+    std::cin>>number2;
+
+
     std::cout<<"o resultado de " << calcula_2_numeros_inteiros(number1,number2,operacao);
 
 
